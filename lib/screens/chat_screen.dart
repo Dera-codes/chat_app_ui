7cbff43bf22786d9f1c3fa668bcefb249941d44a
Nnamdi Chidera Emmanuel <69612512+Dera-codes@users.noreply.github.com>
2023-08-23 10:07:21 +0100

    Final app update

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -60,19 +60,50 @@ class _ChatScreenState extends State<ChatScreen> {
     }
     return Row(
       children: [
-        !isMe
-            ? IconButton(
-                icon: message.isLiked
-                    ? Icon(Icons.favorite)
-                    : Icon(Icons.favorite_border),
-                iconSize: 30,
-                color: message.isLiked
-                    ? Theme.of(context).primaryColor
-                    : Colors.blueGrey,
-                onPressed: () {},
-              )
-            : Text('h')
+        msg,
+        IconButton(
+          icon: message.isLiked
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          iconSize: 30,
+          color: message.isLiked
+              ? Theme.of(context).primaryColor
+              : Colors.blueGrey,
+          onPressed: () {},
+        )
       ],
+    );
+  }
+
+  buildMessageComposer() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      height: 70,
+      color: Colors.white,
+      child: Row(
+        children: [
+          IconButton(
+            icon: Icon(Icons.photo),
+            iconSize: 25,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+          Expanded(
+            child: TextField(
+              textCapitalization: TextCapitalization.sentences,
+              onChanged: (value) {},
+              decoration:
+                  InputDecoration.collapsed(hintText: 'Send a message. . .'),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            iconSize: 25,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 
@@ -99,36 +130,40 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
                 ),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-                child: ListView.builder(
-                  reverse: true,
-                  padding: EdgeInsets.only(top: 15),
-                  itemCount: messages.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final Message message = messages[index];
-                    final bool isMe = message.sender.id == currentUser.id;
-                    return buildMessage(message, isMe);
-                  },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                  child: ListView.builder(
+                    reverse: true,
+                    padding: EdgeInsets.only(top: 15),
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Message message = messages[index];
+                      final bool isMe = message.sender.id == currentUser.id;
+                      return buildMessage(message, isMe);
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            buildMessageComposer(),
+          ],
+        ),
       ),
     );
   }
